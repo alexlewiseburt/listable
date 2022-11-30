fa7bb327fb721ba14b707b9cea5c63bacdc5c659
Alexandrea Burt <alexlewiseburt@gmail.com>
2022-11-30 00:59:29 -0500

    added homescreen

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,32 @@
-import { Text } from "@chakra-ui/react";
+import {
+  Button,
+  Center,
+  Heading,
+  HStack,
+  Link,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import colors from "../src/colors";
+import PinkLink from "../src/components/pink-link";
 
 const HomePage = () => {
-  return <Text>Home Page</Text>;
+  return (
+    <Center h="100vh" bg="gray.200" bgImg="background.jpg" bgPosition="center">
+      <VStack bg="white" rounded="lg" p="5" spacing="4">
+        <Heading color={colors.pink} size="sm">
+          Make a list for any stage in life
+        </Heading>
+        <Heading size="lg" fontWeight="semibold">
+          Online list creator for everyone, including you!
+        </Heading>
+        <HStack>
+          <PinkLink href="/create">Create a list</PinkLink>
+          <PinkLink href="/lists">Find a list</PinkLink>
+        </HStack>
+      </VStack>
+    </Center>
+  );
 };
 
 export default HomePage;
